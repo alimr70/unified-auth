9df8e665dffa79cfba551a9360b9c67dd011b940
Ali Maher <ali.maher.mohammed@gmail.com>
2021-05-31 01:10:23 +0200

    Blanck page on phones fix

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,16 +2,22 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    process.env.NODE_ENV === "production"
-      ? ""
-      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(thunk))
 );
+
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     process.env.NODE_ENV === "production"
+//       ? ""
+//       : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//           window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
 
